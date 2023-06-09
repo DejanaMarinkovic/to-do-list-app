6ee1d3b00186ab079ec0cde7773fde00fa836a3a
Dejana Marinković <118027271+DejanaMarinkovic@users.noreply.github.com>
2023-06-09 10:33:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,49 @@ function addTask() {
     return;
   }
 
-  const taskItem = document.createElement('li');
-  taskItem.textContent = taskText;
+  const taskItem = createTaskElement(taskText);
   taskList.appendChild(taskItem);
 
   taskInput.value = ''; // Resetujemo polje za unos
 }
+
+// Funkcija za kreiranje elementa zadatka
+function createTaskElement(taskText) {
+  const taskItem = document.createElement('li');
+  taskItem.textContent = taskText;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.addEventListener('click', function() {
+    taskItem.remove();
+  });
+
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.addEventListener('click', function() {
+    editTask(taskItem);
+  });
+
+  const completeCheckbox = document.createElement('input');
+  completeCheckbox.type = 'checkbox';
+  completeCheckbox.addEventListener('change', function() {
+    taskItem.classList.toggle('completed');
+  });
+
+  taskItem.appendChild(deleteButton);
+  taskItem.appendChild(editButton);
+  taskItem.appendChild(completeCheckbox);
+
+  return taskItem;
+}
+
+// Funkcija za izmenu zadatka
+function editTask(taskItem) {
+  const taskText = taskItem.textContent;
+  const newTaskText = prompt('Edit task:', taskText);
+
+  if (newTaskText !== null) {
+    taskItem.textContent = newTaskText;
+  }
+}
+
